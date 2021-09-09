f5682449025c452f238ddb0af1d1e1858901deea
Kirill Kovalev <kirilkovalev@yandex.ru>
2021-09-09 23:48:51 +0300

    * | Build; dockerCompose | changed image name and tags; added tag with build number

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 call mvn clean package
-docker build --tag core .
+docker build -t schedapp/core:latest -t schedapp/core:`git rev-list HEAD | wc -l | tr -d ' '` .
 docker-compose up
 docker image prune -a
