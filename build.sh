82003f40a4893b1492287b13654ea9982fbb2ede
Kirill Kovalev <kirilkovalev@yandex.ru>
2021-09-11 13:38:15 +0300

    * | Build | added branch to version

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
-call mvn clean package
-docker build -t schedapp/core:latest -t schedapp/core:`git rev-list HEAD | wc -l | tr -d ' '` .
+#!/bin/bash
+mvn clean package
+
+buildBranch=$(git rev-parse --abbrev-ref HEAD)
+buildNumber=$(git rev-list HEAD | wc -l | tr -d ' ')
+buildVersion="$buildBranch.$buildNumber"
+
+echo Y | docker image prune -a
+docker build -t schedapp/core:latest -t schedapp/core:$buildVersion .
 mkdir ./target/core
-docker save -o ./target/core/image.tar schedapp/core:latest
+echo Y | docker image prune
+docker save -o ./target/core/image.tar schedapp/core
 cp ./docker-compose.yml ./target/core/docker-compose.yml
-docker-compose up
-docker image prune -a
